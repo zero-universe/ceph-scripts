7eee828b79428085308d2c8813bc08f14621527d
Oleg Lusan <zero.universe@gmail.com>
2017-09-13 15:52:23 +0200

    added 002.5_ceph_mgr_add.sh for ceph 12.1 mgr setup

---
+++ 002.5_ceph_mgr_add.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o noclobber
+set -o noglob
+
+if [ $# -ne 1 ]; then
+        echo "Usage: $0 <cluster_name>"
+        echo "Example: $0 ceph-test"
+        exit 1
+fi
+
+
+CEPH=$(which ceph)
+
+# go to ceph config dir
+CPWD="/etc/ceph"
+CLUSTER_CONF="${CPWD}/${CLUSTER_NAME}.conf"
+
+CLUSTER_NAME=$1
+
+# create mgr.keyring and copy it to the right place
+${CEPH} --cluser ${CLUSTER_NAME} auth get-or-create mgr.$(hostname -s) mon 'allow profile mgr' osd 'allow *' mds 'allow *' -o /etc/ceph/${CLUSTER_NAME}.mgr.$(hostname -s).keyring
+mkdir -p /var/lib/ceph/mgr/${CLUSTER_NAME}-$(hostname -s)
+cp /etc/ceph/${CLUSTER_NAME}.mgr.$(hostname -s).keyring /var/lib/ceph/mgr/${CLUSTER_NAME}-$(hostname -s)/
+
+systemctl enable ceph-mgr@$(hostname -s).service
+
+exit 0
