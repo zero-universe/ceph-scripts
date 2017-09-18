4be8055784e9b36c5dc79dbe739104409689a90e
Oleg Lusan <zero.universe@gmail.com>
2017-09-18 07:47:14 +0200

    added script for adding ceph-mgr

--- 002.5_ceph_mgr_add.sh
+++ 002.5_ceph_mgr_add.sh
@@ -14,16 +14,18 @@ fi
 
 CEPH=$(which ceph)
 
+CLUSTER_NAME=$1
+
 # go to ceph config dir
 CPWD="/etc/ceph"
 CLUSTER_CONF="${CPWD}/${CLUSTER_NAME}.conf"
 
-CLUSTER_NAME=$1
 
 # create mgr.keyring and copy it to the right place
-${CEPH} --cluser ${CLUSTER_NAME} auth get-or-create mgr.$(hostname -s) mon 'allow profile mgr' osd 'allow *' mds 'allow *' -o /etc/ceph/${CLUSTER_NAME}.mgr.$(hostname -s).keyring
+${CEPH} --cluster ${CLUSTER_NAME} auth get-or-create mgr.$(hostname -s) mon 'allow profile mgr' osd 'allow *' mds 'allow *' -o /etc/ceph/${CLUSTER_NAME}.mgr.$(hostname -s).keyring
 mkdir -p /var/lib/ceph/mgr/${CLUSTER_NAME}-$(hostname -s)
-cp /etc/ceph/${CLUSTER_NAME}.mgr.$(hostname -s).keyring /var/lib/ceph/mgr/${CLUSTER_NAME}-$(hostname -s)/
+cp /etc/ceph/${CLUSTER_NAME}.mgr.$(hostname -s).keyring /var/lib/ceph/mgr/${CLUSTER_NAME}-$(hostname -s)/keyring
+chown -R ceph. /var/lib/ceph/mgr/
 
 systemctl enable ceph-mgr@$(hostname -s).service
 
